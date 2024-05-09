73618ad716437684b5d705cd91c28e993ec23d84
OnShubham <shubhamkharche3005@gmail.com>
2024-05-09 20:09:31 +0530

    docker deployment

--- frontend/src/store/auth.js
+++ frontend/src/store/auth.js
@@ -40,7 +40,10 @@
 
 import { createContext, useState, useEffect } from 'react';
 
+
+
 export const AuthContext = createContext();
+
 
 export const AuthProvider = ({ children }) => {
     const [token, setToken] = useState(localStorage.getItem('token') || "");
